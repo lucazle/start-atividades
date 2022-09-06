869bf57d1b78736a38f785ca994b8a11b65e22d1
Lucas Souza <lucasamarosouza1@gmail.com>
2022-09-06 18:36:14 -0300

    inital commit

--- módulo REACT e Docker/Introdução a REACT e virtual DOM/my-app/src/App.js
+++ módulo REACT e Docker/Introdução a REACT e virtual DOM/my-app/src/App.js
@@ -1,22 +1,17 @@
-import './App.css';
-import coruja from "./assets/img/coruja.webp";
+import React, { Component } from "react";
+import "./App.css";
 
+export default class Mensagem extends Component {
+  state = {
+    linguagem: "React Js"
+  };
 
-function App() {
+  render() {
     return (
-        <div className="App">
-            <h1>Aprendendo React</h1>
-            <div>
-                <img id="coruja"
-                    src={coruja}
-                    width="40%"
-                    border-radius="10%"/>
-                <div id="descricao">
-                    <p>Por influência da mitologia grega, tanto que Atena, deusa da guerra e da sabedoria, tinha uma coruja como mascote. Os gregos consideravam a noite como o momento do pensamento filosófico e da revelação intelectual e a coruja, por ser uma ave noturna, acabou representando essa busca pelo saber.</p>
-                </div>
-            </div>
-        </div>
+      <div className="App">
+        <h1>Olá mundo {this.state.linguagem}</h1>
+        <h2>Esse é meu componente de classe :]</h2>
+      </div>
     );
+  }
 }
-
-export default App;
